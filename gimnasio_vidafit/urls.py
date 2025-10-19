10e48ad52efabbbb34bd1449243eb3b9e0c3c6ac
Gomartinez10 <gonzalojmartinez30@gmail.com>
2025-10-19 16:39:44 -0300

    agregando mas cosas

--- gimnasio_vidafit/urls.py
+++ gimnasio_vidafit/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from gimnasio_vidafit.views import index
+from gimnasio_vidafit.views import index, registro
 
 urlpatterns = [
     path("", index , name="index"),
-    
+    path("registro/", registro, name="registro")
 ]
--- gimnasio_vidafit/views.py
+++ gimnasio_vidafit/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, "gimnasio_vidafit/index.html")
     
+def registro(request):
+    return render(request, "gimnasio_vidafit/registro.html")
