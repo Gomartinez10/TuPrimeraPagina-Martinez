1cf5e80f413fa95cbb2c1a24be13e7e7a78eff92
Gomartinez10 <gonzalojmartinez30@gmail.com>
2025-10-19 15:33:40 -0300

    modificaciones de html

--- gimnasio_vidafit/models.py
+++ gimnasio_vidafit/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Asociado(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField
+    fecha_de_nacimiento = models.DateField(null=True)
+    
+    def __str__(self):
+        return f"Asociado: {self.nombre} {self.apellido}"
