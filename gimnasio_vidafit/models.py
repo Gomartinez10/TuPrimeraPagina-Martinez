0de39468e20655f2503e5b07e410377a8243d276
Gomartinez10 <gonzalojmartinez30@gmail.com>
2025-10-20 16:39:29 -0300

    default

--- gimnasio_vidafit/models.py
+++ gimnasio_vidafit/models.py
@@ -14,7 +14,7 @@ class Profesional(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     email = models.EmailField(unique=True, blank=True, null=True)
-    especialidad = models.CharField(max_length=100)
+    especialidad = models.CharField(max_length=100, blank=True, null=True)
     fecha_de_nacimiento = models.DateField(null=True)
 
     def __str__(self):
