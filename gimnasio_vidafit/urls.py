575a40b7120d5d9fb0a78bb313e9b3357c3e76d3
Gomartinez10 <gonzalojmartinez30@gmail.com>
2025-10-20 17:15:23 -0300

    añadimos form de clases

--- gimnasio_vidafit/urls.py
+++ gimnasio_vidafit/urls.py
@@ -4,6 +4,7 @@ from gimnasio_vidafit.views import *
 urlpatterns = [
     path("", index , name="index"),
     path("registro/", registro, name="registro"),
-    path("registro_profesional/", registro_profesional, name="registro_profesional")
+    path("registro_profesional/", registro_profesional, name="registro_profesional"),
+    path("agendar_clases/", agendar_clase, name= "agendar_clase")
     
 ]
